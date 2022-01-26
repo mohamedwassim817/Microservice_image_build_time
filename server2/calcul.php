d0b4bd6ebcfd74307d0ee851e916fc2f0c9a16c8
root <root@node1.openshift.com>
2022-01-26 08:48:25 -0800

    first-commit-

---
+++ server2/calcul.php
@@ -0,0 +1,16 @@
+<?php
+
+$worker1="server2";
+
+
+echo("nom du serveur : $worker1");
+/*
+$File = "/tmp/a.html";
+$Handle = fopen($File, 'w');
+$Data1 = "<h1>$worker1</h1>";
+fwrite($Handle, $Data1);
+fclose($Handle);
+*/
+
+?>
+
